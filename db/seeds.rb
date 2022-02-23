5865e27bd4c4e3749c591d6b718a63c8c6dd0ff8
millene prado <millene_vprado@hotmail.com>
2022-02-22 22:32:02 -0300

    created flat model, controller, index view & seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Mount Camel Flat London',
+  address: '15 Clifton Gardens London W9 1DT',
+  description: 'A lovely place to stay',
+  price_per_night: 80,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'St Pancras Clock Tower Guest Suite',
+  address: 'Euston Rd London N1C 4QP',
+  description: 'A very nice place in london',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'My Crib',
+  address: 'Euston Rd London N1C 5QP',
+  description: 'Best place to stay with friends',
+  price_per_night: 150,
+  number_of_guests: 6
+)
+
+puts "Finished!"
